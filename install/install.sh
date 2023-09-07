00fc5db3cdec60a7f3854dc02497adaf43962d52
one <capone.andrea195@gmail.com>
2023-09-07 14:24:59 +0200

    aggiunto script install

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 wget https://github.com/milvus-io/milvus/releases/download/v2.2.12/milvus-standalone-docker-compose.yml -O docker-compose.yml
 sudo docker compose up -d
-docker run --restart always --publish=7474:7474 --publish=7687:7687 --env NEO4J_AUTH=neo4j/marioandrea
+docker run --name Neo4JNLP -d --publish=7474:7474 --publish=7687:7687 --env NEO4J_AUTH=neo4j/marioandrea
 git clone https://github.com/andrewbighead/NLP_Project.git
 virtualenv nlp
 source nlp/bin/activate
